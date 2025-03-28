8e7f4b17d12ea9821a7054e0ca892c32218c981b
AlejoGomezDev <alejongomez@gmail.com>
2025-03-28 15:49:07 -0300

    v7

--- Portfolio/src/components/Molecules/Project.tsx
+++ Portfolio/src/components/Molecules/Project.tsx
@@ -15,51 +15,65 @@ interface Tech {
   iconUrl: string;
 }
 
-
 export function Project({
   title,
   description,
   imageUrl,
   techs,
   Content,
-}: ProjectProps) 
-{
+}: ProjectProps) {
+  const [expand, setExpand] = useState(false);
 
-  const [expand,setExpand] = useState(false)
-
-
-
-
+  const handleClick = () =>{
+    setExpand(!expand)
+  }
 
   return (
-    <div id="individual-project" 
-    className="
-    project text-center p-5 flex 
-    flex-col gap-5 
+
+    <div
+      id="individual-project"
+      className="
     bg-white/1
-    cursor-pointer 
-    hover:scale-102 
+    cursor-pointer
+    hover:scale-102
     hover:brightness-120
-    transition-all 
-    duration-300 
-    ease-in-out">
-      
-      <h1 className="text-2xl">{title}</h1>
+    transition-all
+    duration-300
+    ease-in-out
+    project text-center p-5"
+    onClick={handleClick}
+    >
+      {!expand ? (
+        <div
+          className="
+        flex
+        flex-col gap-5"
+        
+        >
+          <h1 className="text-2xl">{title}</h1>
 
-      <img src={imageUrl} alt={title} className="w-100 h-50"/>
+          <img src={imageUrl} alt={title} className="w-100 h-50" />
 
-      <div className="flex w-1/1 justify-around">
-        {
-          techs.map( ( tech,index ) =>{
-            const {alt,iconUrl} = tech
-            return(
-              <img key={index} src={iconUrl} alt={alt} className="h-8 w-auto" />
-            )
-          })
-        }
-      </div>
+          <div className="flex w-1/1 justify-around">
+            {techs.map((tech, index) => {
+              const { alt, iconUrl } = tech;
+              return (
+                <img
+                  key={index}
+                  src={iconUrl}
+                  alt={alt}
+                  className="h-8 w-auto"
+                />
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>{title}</h1>
 
+        </div>
+      )}
     </div>
   );
 }
-
